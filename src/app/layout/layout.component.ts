8146e85e7ae32adb524421d38ef6fbf59a5ae10c
Cristian Cajiao <cristiancajiaos@outlook.com>
2023-02-01 11:58:03 -0300

    Definido layout básico

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
+  public numbers: number[] = [5, 14, 22, 3, 7, 0, 9, 18, 45, 2, 12, 31, 8, 22];
   constructor() { }
 
   ngOnInit() {
